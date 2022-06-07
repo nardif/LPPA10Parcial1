b61ff70c870c47204668c8e5842ff5d78cccee40
nardif <shopinardi@hotmail.com>
2022-06-06 23:58:11 -0300

    Onceavo Commit: agregado de Readme y detalles de formato en el script

---
+++ README.md
@@ -0,0 +1,24 @@
+Consigna:
+Desarrollar una página web accesible desde cualquier navegador web utilizando solo las tecnologías: HTML,
+CSS y Javascript.
+
+La página debe tener diseño responsive adaptable a una pantalla Desktop y Mobile, utilizando Flexbox para
+crear contenidos adaptables y evitar el uso de media queries, solo usarlo para casos puntuales. No se deben
+usar librerías externas para diseños, todo se debe hacer solo con CSS. El HTML, CSS y JS desarrollados
+deben estar en archivos diferentes. No usar estilos en línea.
+
+Se debe crear una página web con HTML utilizando las etiquetas semánticas correspondientes, siguiendo la
+estructura de Layout sugerida por W3School. La página debe contener un formulario de registro con los
+siguientes campos y validaciones:
+● Nombre (texto corto no menor a 3 letras)
+● Apellido (texto corto no menor a 3 letras)
+● Email (validar formato de email usando regex /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/)
+● Edad (número entero mayor a 0 y menor a 100)
+● Sexo (opciones múltiples de selección simple)
+● Temas de Interés - Deporte, música, juegos, tecnología, otros (opciones múltiples de selección
+múltiple)
+● País - Argentina, Chile, Brasil, Uruguay (menú desplegable)
+● Comentario (texto largo)
+● Botón enviar
+
+Todos los campos son requeridos excepto el campo de “comentario”.
--- script.js
+++ script.js
@@ -2,107 +2,108 @@ window.onload = function() {
 
     //VALIDAR SUBMIT
     document.getElementById("formRegister").addEventListener('submit', validateForm);
- 
     var successMsg = false;
+    
     //INPUTS RETRIVE
-var fname = document.getElementById('name');
-var lastname = document.getElementById('lastname');
-var mail = document.getElementById('email');
-var age = document.getElementById('age');
-var gender = document.getElementsByName('gender');
-var interests = document.getElementsByName('interests');
+    var fname = document.getElementById('name');
+    var lastname = document.getElementById('lastname');
+    var mail = document.getElementById('email');
+    var age = document.getElementById('age');
+    var gender = document.getElementsByName('gender');
+    var interests = document.getElementsByName('interests');
 
-  //ERROR MESSAGES
-var em_name = document.getElementById('em_name');
-var em_lastname = document.getElementById('em_lastname');
-var em_email = document.getElementById('em_email');
-var em_age = document.getElementById('em_age');
-var em_gender = document.getElementById('em_gender');
-var em_interests = document.getElementById('em_interests');
+    //ERROR MESSAGES
+    var em_name = document.getElementById('em_name');
+    var em_lastname = document.getElementById('em_lastname');
+    var em_email = document.getElementById('em_email');
+    var em_age = document.getElementById('em_age');
+    var em_gender = document.getElementById('em_gender');
+    var em_interests = document.getElementById('em_interests');
 
-//VALIDATION FUNCTIONS
-//Name and lastname
-function validateNames(nln,em){
-    // if (fname.value.length < 3) {
-    //     em_name.classList.remove('hiddenError');
-    // }
-    if (nln.value.length < 3) {
-        em.classList.remove('hiddenError');
-        successMsg = false;
-    }
-    if (nln.value == null) {
-        em.classList.remove('hiddenError');
-        successMsg = false;
-    }
-}    
-//Email
-function validateEmail(){
-    regex = /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/;
-    if (!regex.test(mail.value)) {
-        em_email.classList.remove('hiddenError');
-        successMsg = false;
-    }
-}
-//Age
-function validateAge(){
-    if (!(age.value >=1) && (age.value<=100)) {
-        em_age.classList.remove('hiddenError');
-        successMsg = false;
-    }
-}
-//gender
-function validateGender(){
-    if ((gender[0].checked==false)&&(gender[1].checked==false)&&(gender[2].checked==false)) {
-        em_gender.classList.remove('hiddenError');
-        successMsg = false;
-    }
-}
-//interests
-function validateInterests(){
-    var t = interests.length;
-    for (let i = 0; i < interests.length; i++) {
-        if(interests[i].checked == false){
-            t--;
+    //VALIDATION FUNCTIONS
+    //Name and lastname
+    function validateNames(nln,em){
+        // if (fname.value.length < 3) {
+        //     em_name.classList.remove('hiddenError');
+        // }
+        if (nln.value.length < 3) {
+            em.classList.remove('hiddenError');
+            successMsg = false;
+        }
+        if (nln.value == null) {
+            em.classList.remove('hiddenError');
+            successMsg = false;
+        }
+    }    
+    //Email
+    function validateEmail(){
+        regex = /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/;
+        if (!regex.test(mail.value)) {
+            em_email.classList.remove('hiddenError');
+            successMsg = false;
         }
     }
-    if (t==0) {
-        em_interests.classList.remove('hiddenError');
-        successMsg = false;
+    //Age
+    function validateAge(){
+        if (!(age.value >=1) && (age.value<=100)) {
+            em_age.classList.remove('hiddenError');
+            successMsg = false;
+        }
     }
-}
+    //gender
+    function validateGender(){
+        if ((gender[0].checked==false)&&(gender[1].checked==false)&&(gender[2].checked==false)) {
+            em_gender.classList.remove('hiddenError');
+            successMsg = false;
+        }
+    }
+    //interests
+    function validateInterests(){
+        var t = interests.length;
+        for (let i = 0; i < interests.length; i++) {
+            if(interests[i].checked == false){
+                t--;
+            }
+        }
+        if (t==0) {
+            em_interests.classList.remove('hiddenError');
+            successMsg = false;
+        }
+    }
 
-function modal(){
-    if (successMsg.value == true) {
-        document.getElementById('modalContainer').style.visibility="visible";
-    }else{
+    function modal(){
+        if (successMsg.value == true) {
+            document.getElementById('modalContainer').style.visibility="visible";
+        }else{
+            document.getElementById('modalContainer').style.visibility="hidden";
+        }
+    }
+    //CLEAR ERROR FUNCTION
+    function clearErrorMsg(){
+        var errorType = "em_" + document.activeElement.name;
+        var errorMsg = document.getElementById(errorType);
+        errorMsg.classList.add('hiddenError');
+    }
+    //CLEANING ERROR EVENTS
+    fname.addEventListener('focus',clearErrorMsg);
+    lastname.addEventListener('focus',clearErrorMsg);
+    mail.addEventListener('focus',clearErrorMsg);
+    age.addEventListener('focus',clearErrorMsg);
+    gender.forEach(gender => gender.addEventListener('change', clearErrorMsg));
+    interests.forEach(interests => interests.addEventListener('change', clearErrorMsg));
+
+    //CALLING ALL VALIDATIONS
+    function validateForm(submition){
+        submition.preventDefault();
         document.getElementById('modalContainer').style.visibility="hidden";
+        validateNames(fname, em_name);
+        validateNames(lastname,em_lastname);
+        validateEmail();
+        validateAge();
+        validateGender();
+        validateInterests();
+        setTimeout( function(){
+            modal();
+        }, 3000);
     }
-}
-//CLEAR ERROR FUNCTION
-function clearErrorMsg(){
-    var errorType = "em_" + document.activeElement.name;
-    var errorMsg = document.getElementById(errorType);
-    errorMsg.classList.add('hiddenError');
-}
-//CLEANING ERROR EVENTS
-fname.addEventListener('focus',clearErrorMsg);
-lastname.addEventListener('focus',clearErrorMsg);
-mail.addEventListener('focus',clearErrorMsg);
-age.addEventListener('focus',clearErrorMsg);
-gender.forEach(gender => gender.addEventListener('change', clearErrorMsg));
-interests.forEach(interests => interests.addEventListener('change', clearErrorMsg));
-
-//CALLING ALL VALIDATIONS
-function validateForm(submition){
-    submition.preventDefault();
-    document.getElementById('modalContainer').style.visibility="hidden";
-    validateNames(fname, em_name);
-    validateNames(lastname,em_lastname);
-    validateEmail();
-    validateAge();
-    validateGender();
-    validateInterests();
-    setTimeout( function(){
-        modal();
-    }, 3000);
 }
