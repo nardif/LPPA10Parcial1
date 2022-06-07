16c40488f9dc7822904a4c3a972b953dd2d23923
nardif <shopinardi@hotmail.com>
2022-06-06 23:46:15 -0300

    Sexto Commit: error messages declaration

--- script.js
+++ script.js
@@ -11,5 +11,12 @@ var age = document.getElementById('age');
 var gender = document.getElementsByName('gender');
 var interests = document.getElementsByName('interests');
 
-  
+  //ERROR MESSAGES
+var em_name = document.getElementById('em_name');
+var em_lastname = document.getElementById('em_lastname');
+var em_email = document.getElementById('em_email');
+var em_age = document.getElementById('em_age');
+var em_gender = document.getElementById('em_gender');
+var em_interests = document.getElementById('em_interests');
+
 }
