6c90eb636d3120f2d57d9579a9d2dcfeb6d8e12f
nardif <shopinardi@hotmail.com>
2022-06-06 23:44:47 -0300

    Quinto Commit: inputs retrive del DOM

--- script.js
+++ script.js
@@ -3,5 +3,13 @@ window.onload = function() {
     //VALIDAR SUBMIT
     document.getElementById("formRegister").addEventListener('submit', validateForm);
  
+    //INPUTS RETRIVE
+var fname = document.getElementById('name');
+var lastname = document.getElementById('lastname');
+var mail = document.getElementById('email');
+var age = document.getElementById('age');
+var gender = document.getElementsByName('gender');
+var interests = document.getElementsByName('interests');
+
   
 }
